95ed7187ce4fddd1dc9f63e8f99c978d52756188
Terence Ho <terenceh@codeaurora.org>
2015-06-25 16:42:43 -0400

    msm: camera: Reduce log level of dropped frame events
    Do not log dropped frames as error to avoid kernel spew for
    expected events.
    
    Change-Id: I209ae11780bba0d0a4a3843c13193a841af5a712
    Signed-off-by: Terence Ho <terenceh@codeaurora.org>

--- drivers/media/video/msm/msm.c
+++ drivers/media/video/msm/msm.c
@@ -364,7 +364,13 @@ static int msm_camera_v4l2_dqbuf(struct file *f, void *pctx,
 	}
 	rc = vb2_dqbuf(&pcam_inst->vid_bufq, pb,  f->f_flags & O_NONBLOCK);
 	if (rc < 0) {
-		pr_err("%s, videobuf_dqbuf returns %d\n", __func__, rc);
+		if (rc == -EAGAIN)
+			pr_debug(
+				"%s, videobuf_dqbuf queue empty %d, flags 0x%X\n",
+				__func__, rc, f->f_flags);
+		else
+			pr_err("%s, videobuf_dqbuf returns %d, flags 0x%X\n",
+				__func__, rc, f->f_flags);
 		mutex_unlock(&pcam_inst->inst_lock);
 		return rc;
 	}
